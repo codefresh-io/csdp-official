d88fcb48b544fdc46b1a7ce14a46a82083f8301b
danielm-codefresh <daniel.maizel@codefresh.io>
2022-04-25 17:18:09 +0300

    last fixes

--- installer/install.sh
+++ installer/install.sh
@@ -14,10 +14,12 @@ check_required_param() {
 
 # Constants:
 CODEFRESH_SECRET_NAME="codefresh-token"
+CODEFRESH_CM_NAME="codefresh-cm"
 REPO_CREDS_SECRET_NAME="autopilot-secret"
 ARGOCD_TOKEN_SECRET_NAME="argocd-token"
 ARGOCD_INITIAL_TOKEN_SECRET_NAME="argocd-initial-admin-secret"
 BOOTSTRAP_APP_NAME="csdp-bootstrap"
+ADDITIONAL_COMPONENTS_MANAGED="\nevents-reporter"
 ADDITIONAL_COMPONENTS="\nevents-reporter\nrollout-reporter\nworkflow-reporter"
 RUNTIME_DEF_URL="https://github.com/codefresh-io/csdp-official-poc/releases/VERSION/download/runtime.yaml"
 
@@ -37,6 +39,7 @@ CSDP_GIT_INTEGRATION_PROVIDER="${CSDP_GIT_INTEGRATION_PROVIDER:-GITHUB}"
 CSDP_GIT_INTEGRATION_API_URL="${CSDP_GIT_INTEGRATION_API_URL:-https://api.github.com}"
 CSDP_GIT_INTEGRATION_TOKEN="${CSDP_GIT_INTEGRATION_TOKEN:-${CSDP_RUNTIME_GIT_TOKEN}}"
 CSDP_RUNTIME_REPO_CREDS_PATTERN=`echo ${CSDP_RUNTIME_REPO} | grep --color=never -E -o '^http[s]?:\/\/([a-zA-Z0-9\.]*)'`
+CSDP_MANAGED_RUNTIME="${CSDP_MANAGED_RUNTIME:-false}"
 
 create_codefresh_secret() {
     # Download runtime definition
@@ -50,12 +53,16 @@ create_codefresh_secret() {
     echo ""
 
     # Prepare components for request
+
+    if [[ "$CSDP_MANAGED_RUNTIME" == "true" ]] ; then
+        ADDITIONAL_COMPONENTS=${ADDITIONAL_COMPONENTS_MANAGED}
+    fi
     COMPONENT_NAMES=`echo "$RUNTIME_DEF" | yq e '.spec.components.[].name' -`
     COMPONENT_NAMES=`printf "${COMPONENT_NAMES}${ADDITIONAL_COMPONENTS}" | tr '\n' ' '`
-    COMPONENTS="[\"argo-cd\""
+    COMPONENTS="[\"csdp-argo-cd\""
     for COMPONENT in $COMPONENT_NAMES
     do
-        CUR_COMPONENT=`echo -n "\"${CSDP_RUNTIME_NAME}-${COMPONENT}\""`
+        CUR_COMPONENT=`echo -n "\"csdp-${COMPONENT}\""`
         COMPONENTS="${COMPONENTS},${CUR_COMPONENT}"
     done
     COMPONENTS="${COMPONENTS}]"
@@ -83,6 +90,13 @@ create_codefresh_secret() {
     --compressed \
     --insecure \
     --data-raw "$RUNTIME_CREATE_DATA"`
+
+    if `echo "$RUNTIME_CREATE_RESPONSE" | jq -e 'has("errors")'`; then
+        echo "Failed to create runtime"
+        echo ${RUNTIME_CREATE_RESPONSE}
+        exit 1
+    fi
+
     RUNTIME_ACCESS_TOKEN=`echo $RUNTIME_CREATE_RESPONSE | jq '.data.createRuntime.newAccessToken'`
     RUNTIME_ENCRYPTION_IV=`hexdump -n 16 -e '4/4 "%08x" 1 "\n"' /dev/urandom`
     echo "  --> Runtime created!"
@@ -163,6 +177,24 @@ create_repo_creds_secret() {
     " | kubectl apply -f -
 }
 
+create_managed_repo_creds_secret() {
+    echo " --> Creating managed repo credentials secret"
+    echo "
+    apiVersion: v1
+    kind: Secret
+    metadata:
+        labels:
+            argocd.argoproj.io/secret-type: repository
+        name: $REPO_CREDS_SECRET_NAME
+        namespace: $NAMESPACE
+    stringData:
+        type: git
+        url: $CSDP_RUNTIME_REPO
+        password: $CSDP_RUNTIME_GIT_TOKEN
+        username: username
+    " | kubectl apply -f -
+}
+
 create_argocd_token_secret() {
     echo "  --> Reading ArgoCD intial admin token..."
     INITIAL_PASSWORD=`kubectl -n ${NAMESPACE} get secret ${ARGOCD_INITIAL_TOKEN_SECRET_NAME} -o=jsonpath="{.data.password}" | base64 -d`
@@ -214,7 +246,9 @@ create_git_integration() {
     echo "  --> Created git integration:"
     echo "${GIT_INTEGRATION_CREATE_RESPONSE}"
     echo ""
+}
 
+register_to_git_integration() {
     echo "  --> Registering user to default git integration"
 
     GIT_INTEGRATION_REGISTER_ARGS="{
@@ -233,7 +267,13 @@ create_git_integration() {
     --insecure \
     --data-raw "$GIT_INTEGRATION_REGISTER_DATA"`
 
-    echo "  --> Register to default git integration:"
+    if `echo "$GIT_INTEGRATION_REGISTER_RESPONSE" | jq -e 'has("errors")'`; then
+        echo "Failed to register git integration"
+        echo ${GIT_INTEGRATION_REGISTER_RESPONSE}
+        exit 1
+    fi
+
+    echo "  --> Registered to default git integration:"
     echo "${GIT_INTEGRATION_REGISTER_RESPONSE}"
     echo "" 
 }
@@ -255,6 +295,7 @@ echo "  runtime git-token: ****"
 echo "  runtime cluster: ${CSDP_RUNTIME_CLUSTER}"
 echo "  runtime name: ${CSDP_RUNTIME_NAME}"
 echo "  runtime version: ${CSDP_RUNTIME_VERSION}"
+echo "  managed runtime: ${CSDP_MANAGED_RUNTIME}"
 echo "  runtime ingress: ${CSDP_RUNTIME_INGRESS_URL}"
 echo "  ingress class name: ${CSDP_INGRESS_CLASS_NAME}"
 echo "  ingress controller: ${CSDP_INGRESS_CONTROLLER}"
@@ -280,7 +321,12 @@ if kubectl -n "$NAMESPACE" get secret "$REPO_CREDS_SECRET_NAME"; then
 else
     echo "  --> Secret $REPO_CREDS_SECRET_NAME doesn't exists."
     echo ""
-    create_repo_creds_secret
+
+    if [[ "$CSDP_MANAGED_RUNTIME" == "true" ]] ; then
+        create_managed_repo_creds_secret
+    else
+        create_repo_creds_secret
+    fi
 fi
 echo ""
 echo ""
@@ -309,6 +355,11 @@ if kubectl -n "$NAMESPACE" get secret -l io.codefresh.integration-type=git -l io
     create_git_integration
 else
     echo "  --> Default git integration exists"
+fi
+
+# 6. Register to git integration
+if [[ "$CSDP_MANAGED_RUNTIME" == "true" ]] ; then
+    register_to_git_integration
 fi
 echo ""
 
