44d6d6e2ccaa4d3d9819a12e3d146381a442130e
kim-codefresh <kim.aharfi@codefresh.io>
2022-09-18 09:20:41 +0300

    make sure in case kubectl exec fail the whole proccess will fail, for delete secret just warning

--- add-cluster/add-cluster.sh
+++ add-cluster/add-cluster.sh
@@ -11,7 +11,7 @@
 SECRET_NAME=""
 
 function get_service_account_secret_name() {
-  SECRET_NAME=$(kubectl get ServiceAccount ${SERVICE_ACCOUNT_NAME} -n ${NAMESPACE} -o jsonpath='{.secrets[0].name}')
+  SECRET_NAME=$(kubectl get ServiceAccount ${SERVICE_ACCOUNT_NAME} -n ${NAMESPACE} -o jsonpath='{.secrets[0].name}') || exit 1
   if [[ -z ${SECRET_NAME} ]]; then
     echo "Creating new ServiceAccount token"
     # create secret for service account
@@ -24,9 +24,9 @@ metadata:
     kubernetes.io/service-account.name: ${SERVICE_ACCOUNT_NAME}
 type: kubernetes.io/service-account-token
 EOF
-)
+) || exit 1
     SECRET_NAME=$(echo ${SECRET_NAME} | sed s@secret/@@g | sed s/\ created//g)
-    kubectl patch ServiceAccount ${SERVICE_ACCOUNT_NAME} -n ${NAMESPACE} --patch "{\"secrets\": [{\"name\": \"${SECRET_NAME}\"}]}"
+    kubectl patch ServiceAccount ${SERVICE_ACCOUNT_NAME} -n ${NAMESPACE} --patch "{\"secrets\": [{\"name\": \"${SECRET_NAME}\"}]}" || exit 1
     echo "Created ServiceAccount sercret ${SECRET_NAME}"
   else
     echo "Found ServiceAccount secret ${SECRET_NAME}"
@@ -53,10 +53,11 @@ BEARER_TOKEN=$(kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o jsonpath='{.
 
 # write KUBE_COPNFIG_DATA to local file
 CLUSTER_NAME=$(echo ${SERVER} | sed s/'http[s]\?:\/\/'//)
-kubectl config set-cluster "${CLUSTER_NAME}" --server="${SERVER}" --certificate-authority="${CACERT}"
-kubectl config set-credentials "${SERVICE_ACCOUNT_NAME}" --token "${BEARER_TOKEN}"
-kubectl config set-context "${CONTEXT_NAME}" --cluster="${CLUSTER_NAME}" --user="${SERVICE_ACCOUNT_NAME}"
-KUBE_CONFIG_B64=$(kubectl config view --minify --flatten --output json --context="${CONTEXT_NAME}" | base64 -w 0)
+kubectl config set-cluster "${CLUSTER_NAME}" --server="${SERVER}" --certificate-authority="${CACERT}" || exit 1
+kubectl config set-credentials "${SERVICE_ACCOUNT_NAME}" --token "${BEARER_TOKEN}" || exit 1
+kubectl config set-context "${CONTEXT_NAME}" --cluster="${CLUSTER_NAME}" --user="${SERVICE_ACCOUNT_NAME}" || exit 1
+KUBE_CONFIG=$(kubectl config view --minify --flatten --output json --context="${CONTEXT_NAME}") || exit 1
+KUBE_CONFIG_B64=$($KUBE_CONFIG | base64 -w 0)
 
 ANNOTATIONS_B64=$(cat /etc/config/annotations.yaml | base64 -w 0)
 LABELS_B64=$(cat /etc/config/labels.yaml | base64 -w 0)
