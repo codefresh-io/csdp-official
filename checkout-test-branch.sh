9891aff74393ce92958da7cc6750341591aaf13b
daniel soifer <daniel.soifer@codefresh.io>
2023-05-28 12:43:08 +0300

    modified checkout-test-branch.sh

--- checkout-test-branch.sh
+++ checkout-test-branch.sh
@@ -1,17 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-export TARGET_VERSION=0.1.30
-export PRE_RELEASE_VERSION=$TARGET_VERSION-0
+if [ $# -ne 2 ]; then
+  echo "Please provide exactly two arguments."
+  echo "Usage: ./checkout-test-branch.sh \$target_version \$prerelease_version_suffix"
+  echo "Example: ./checkout-test-branch.sh 0.2.5 rc1"
+  exit 1
+fi
+
+export TARGET_VERSION=$1
+export PRE_RELEASE_VERSION_SUFFIX=$2
+export PRE_RELEASE_VERSION=$TARGET_VERSION-$PRE_RELEASE_VERSION_SUFFIX
 export TAG=$PRE_RELEASE_VERSION
 export TEST_BRANCH=test-release-$TARGET_VERSION
 
-git checkout -b $TEST_BRANCH
+git checkout -b "$TEST_BRANCH"
 
-echo $PRE_RELEASE_VERSION > VERSION
+echo "$PRE_RELEASE_VERSION" > VERSION
 make bump
 
 git add .
 git commit -m "testing release $TARGET_VERSION"
-git push --set-upstream origin $TEST_BRANCH
+git push --set-upstream origin "$TEST_BRANCH"
 git tag "$TAG"
-git push origin tags/$TAG
+git push origin tags/"$TAG"
