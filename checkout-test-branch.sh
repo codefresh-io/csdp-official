c073366d3da700c93ea0feef6bb2b440a9c6382b
daniel soifer <daniel.soifer@codefresh.io>
2023-05-28 12:47:54 +0300

    modified checkout-test-branch.sh

--- checkout-test-branch.sh
+++ checkout-test-branch.sh
@@ -13,6 +13,8 @@ export PRE_RELEASE_VERSION=$TARGET_VERSION-$PRE_RELEASE_VERSION_SUFFIX
 export TAG=$PRE_RELEASE_VERSION
 export TEST_BRANCH=test-release-$TARGET_VERSION
 
+git checkout main
+git pull
 git checkout -b "$TEST_BRANCH"
 
 echo "$PRE_RELEASE_VERSION" > VERSION
